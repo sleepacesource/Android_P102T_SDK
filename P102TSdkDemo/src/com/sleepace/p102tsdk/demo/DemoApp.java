4467eccb021b3633651a180af6e015a302ec09f9
wangyong <smile@sleepace.net>
2021-10-15 15:46:37 +0800

    Android P102T SDK

---
+++ P102TSdkDemo/src/com/sleepace/p102tsdk/demo/DemoApp.java
@@ -0,0 +1,63 @@
+package com.sleepace.p102tsdk.demo;
+
+import com.sleepace.p102tsdk.demo.util.CrashHandler;
+import com.sleepace.sdk.util.SdkLog;
+
+import android.app.Application;
+import android.os.Environment;
+
+
+public class DemoApp extends Application {
+
+    private static DemoApp instance;
+    
+    public StringBuffer logBuf = new StringBuffer();
+
+    public static DemoApp getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        CrashHandler.getInstance().init(this);
+        SdkLog.setLogEnable(true);
+        String logDir = getExternalFilesDir("log").getPath();
+        SdkLog.setLogDir(logDir);
+        SdkLog.setSaveLog(true);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        // 低内存的时候执行
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        // 程序在内存清理的时候执行
+        super.onTrimMemory(level);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
